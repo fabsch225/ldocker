68a4571b6e248567fcd25646c3fc5841a248bcf3
fabian schuller <fbn.schllr@gmail.com>
2024-02-06 15:17:55 +0100

    First Commit

---
+++ .history/index_20240112162433.mjs
@@ -0,0 +1,83 @@
+import { spawn } from "child_process";
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+
+const API_TOKEN = process.env.API_TOKEN
+  ? process.env.API_TOKEN.length
+    ? process.env.API_TOKEN
+    : undefined
+  : undefined;
+const PORT = 8089//Number(process.env.PORT);
+
+if (!PORT) {
+  throw new Error("Error: `PORT` environment variable is not defined.");
+}
+
+const server = http.createServer();
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  const lc0 = spawn("lc0");
+  let isAuthenticated = !API_TOKEN;
+
+  lc0.stdout.on("data", (data) => {
+    const lines = data
+      .toString()
+      .trim()
+      .split("\n")
+      .map((line) => line.trim());
+
+    lines.forEach((line) => {
+      console.debug("[lc0]", line);
+
+      if (isAuthenticated) {
+        ws.send(JSON.stringify({ type: "uci:response", payload: line }));
+      }
+    });
+  });
+
+  ws.on("message", (message) => {
+    try {
+      const messageData = JSON.parse(message);
+      switch (messageData.type) {
+        case "auth:authenticate":
+          console.debug("[authenticate]", messageData.payload);
+
+          if (!API_TOKEN || messageData.payload === API_TOKEN) {
+            isAuthenticated = true;
+            ws.send(JSON.stringify({ type: "auth:authenticated" }));
+          } else {
+            ws.send(JSON.stringify({ type: "auth:unauthenticated" }));
+          }
+          break;
+        case "uci:command":
+          console.debug("[uci:command]", messageData.payload);
+
+          if (isAuthenticated) {
+            lc0.stdin.write(`${messageData.payload}\n`);
+          }
+          break;
+      }
+    } catch (error) {
+      console.error("Error parsing message: ", error);
+    }
+  });
+
+  ws.on("close", () => {
+    lc0.kill();
+  });
+});
+
+server.on("request", (req, res) => {
+  if (req.url === "/check") {
+    res.writeHead(200);
+    res.end("LeelaDocker is up and running.");
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}.`);
+});
